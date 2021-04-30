656ee5a2d259590da2498591821aca5214459649
Rija Muhamad Yazid <rijamyazid@gmail.com>
2021-04-30 12:49:50 +0700

    Ditambahkan : ViewPager di Home

--- app/src/main/java/com/example/jetpack_submissions/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/jetpack_submissions/ui/home/HomeActivity.kt
@@ -3,10 +3,25 @@ package com.example.jetpack_submissions.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jetpack_submissions.R
+import com.example.jetpack_submissions.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        // Bind view
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Memasang ViewPager di Activity
+        val homePagerAdapter = HomePagerAdapter(this, supportFragmentManager)
+        binding.viewPager.adapter = homePagerAdapter
+        binding.tabs.setupWithViewPager(binding.viewPager)
+        supportActionBar?.elevation = 0f
+
     }
+
 }
